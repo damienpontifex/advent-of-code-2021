0c899c3fb34dd5ac78767b348b38480bee89700e
Damien Pontifex <damien.pontifex@gmail.com>
2021-12-29 12:26:38 +0800

    day2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -3,12 +3,15 @@ use nom::Finish;
 mod direction;
 mod submarine;
 use crate::direction::parse_directions;
-use crate::submarine::Submarine;
+use crate::submarine::{Submarine, SubmarineWithAim};
 
 fn main() {
     let input = include_str!("input.txt");
     let output = move_submarine(input);
     println!("part 1 output {}", output);
+
+    let output = move_submarine_with_aim(input);
+    println!("part 2 output {}", output);
 
     // Example of parsing the whole file at once
     let (_rem, output) = parse_directions(input).finish().unwrap();
@@ -26,12 +29,24 @@ fn move_submarine(input: &str) -> i32 {
     submarine.finalize()
 }
 
+fn move_submarine_with_aim(input: &str) -> i32 {
+    let submarine = input
+        .lines()
+        .fold(SubmarineWithAim::default(), |mut submarine, line| {
+            let direction = line.parse().unwrap();
+            submarine.move_in_direction(direction);
+            submarine
+        });
+    submarine.finalize()
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn example_input() {
+    fn submarine() {
         let input = "forward 5
 down 5
 forward 8
@@ -39,5 +54,16 @@ up 3
 down 8
 forward 2";
         assert_eq!(move_submarine(input), 150);
+    }
+
+    #[test]
+    fn submarine_with_aim() {
+        let input = "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2";
+        assert_eq!(move_submarine_with_aim(input), 900);
     }
 }
--- day2/src/submarine.rs
+++ day2/src/submarine.rs
@@ -19,3 +19,28 @@ impl Submarine {
         self.x * self.y
     }
 }
+
+#[derive(Default)]
+pub(crate) struct SubmarineWithAim {
+    x: i32,
+    y: i32,
+    aim: i32,
+}
+
+impl SubmarineWithAim {
+    pub(crate) fn move_in_direction(&mut self, direction: Direction) {
+        match direction {
+            Direction::Forward(magnitude) => {
+                self.x += magnitude;
+                self.y += self.aim * magnitude;
+            },
+            Direction::Up(magnitude) => self.aim -= magnitude,
+            Direction::Down(magnitude) => self.aim += magnitude,
+        }
+    }
+
+    pub(crate) fn finalize(&self) -> i32 {
+        self.x * self.y
+    }
+}
+
